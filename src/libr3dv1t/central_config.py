2abb0fd17e6a3b456024232da964813de06bbc20
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-03 08:24:36 -0400

    kdf improved

--- src/libr3dv1t/central_config.py
+++ src/libr3dv1t/central_config.py
@@ -8,7 +8,6 @@ Also documentation for the library params might be found here.
 # ----------------------------------------------------------------------------------------------------------------------
 _DEFAULT_CHUNK_SIZE = 2048
 _DEFAULT_REPLICATION = 3
-_R3DV1T_SALT = b"libr3dv1t_salt_4242"
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
@@ -19,7 +18,6 @@ class CentralConfig:
     def __init__(self):
         self.default_replicas = _DEFAULT_REPLICATION
         self.default_chunk_size = _DEFAULT_CHUNK_SIZE
-        self.r3dv1t_salt = _R3DV1T_SALT
 
         # implement any env variable overrides here
 

f7c065df19b179b58773847a9e44a7a30e5c98ff
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-03 08:45:53 -0400

    starting to build a logging system

--- src/libr3dv1t/log_utilz/log_manager.py
+++ src/libr3dv1t/log_utilz/log_manager.py
@@ -1,2 +1,14 @@
+import enum
 
 
+# You can do > < >= <= == comparison on these, ie: lvl_1.value >= lvl_2.value
+# A filter set to LGLVL.DEBUG means filter nothing.
+class LGLVL(enum.Enum):
+    DBUG = 10
+    INFO = 20
+    WARN = 30
+    ERRR = 40
+    CRIT = 50
+
+    def __str__(self):
+        return super().__str__()[6:]  # chopping i.e. "LGLVL.DBUG", to "DBUG"
---
+++ src/libr3dv1t/log_utilz/sqlite_log.py
@@ -0,0 +1,17 @@
+"""
+A simple logging system with a sqlite log sink.
+
+- Code over Configuration. Code is self-describing and powerful.
+- Switch between views to see different log levels.
+
+"""
+
+import sqlite3
+import os
+from pathlib import Path
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+
+
+

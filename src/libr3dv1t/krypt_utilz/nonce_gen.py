0a77e47b005263e28e184ddb3d1bce5080ef4255
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-03 08:31:03 -0400

    nonce generator improved

--- src/libr3dv1t/krypt_utilz/nonce_gen.py
+++ src/libr3dv1t/krypt_utilz/nonce_gen.py
@@ -1,7 +1,7 @@
 import os
 import time
-from hashlib import pbkdf2_hmac, sha3_384
-from libr3dv1t.central_config import default_rvcc as _rvcc
+from hashlib import pbkdf2_hmac as _pbkdf2_hmac
+from hashlib import sha3_384 as _sha3_384
 from libr3dv1t.errors import R3D_V1T_Error
 
 
@@ -16,11 +16,11 @@ def make_nonce(size: int) -> bytes:
         raise R3D_V1T_Error("Nonce less than 16 bytes is not allowed.")
 
     rand_bytes_kernel = os.urandom(80)  # 80 bytes = 640 bits
-    rand_bytes_time = sha3_384(str(time.time_ns() * time.time_ns()).encode('ascii')).digest()
+    rand_bytes_time = _sha3_384(str(time.time_ns() * time.time_ns()).encode('ascii')).digest()
 
-    combined_rand = sha3_384(rand_bytes_kernel + rand_bytes_time).digest()
+    combined_rand = _sha3_384(rand_bytes_kernel + rand_bytes_time).digest()
 
     # PBKDF2 is not necessary here but its convinient to supported any requested length
-    nonce = pbkdf2_hmac(hash_name='sha3_512', password=combined_rand, salt=_rvcc.r3dv1t_salt, iterations=8, dklen=size)
+    nonce = _pbkdf2_hmac(hash_name='sha3_512', password=combined_rand, salt=b"xcf__dont_care__qwk", iterations=8, dklen=size)
 
     return nonce

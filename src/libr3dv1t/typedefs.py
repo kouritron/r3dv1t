5f0073bacb168c01cffe861338bad5012397c20b
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-03 23:59:35 -0400

    vobj to memobj refactoring just starting

--- src/libr3dv1t/typedefs.py
+++ src/libr3dv1t/typedefs.py
@@ -1,19 +1,37 @@
+''' typedefs.py
+
+structs, typedefs, dataclasses, enums .... used in the libr3dv1t library.
+
+Try to avoid logic in this file as much as possible. Most of the classes here are to be treated
+as data containers/data classes, not grouping of data and logic.
+
+
+'''
+
 from enum import Enum
 
 
-# ------------------------------------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------------------------------------
-class VaultObj:
-    """ Data class ish tracking all object attributes that r3dv1t can track.
-    Try to use it as a data class and ideally avoid storing logic in this class. """
+_ = """
+TODO:
 
-    def __init__(self, vpn: str = "", obj_mtime: float = 0.0, obj_id: str = ""):
-        # vpn: vault path name, this is the name of the object in the vault.
-        self.vpn = vpn
-        self.obj_mtime = obj_mtime
-        self.obj_id = obj_id
-        self.pt_data = None
-        self.ct_segments = []
+deal with 
+- vpn or vname: vault path name
+- same place should handle obj_mtime
+
+
+
+"""
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+class MemObj:
+    """ Data class ish tracking a r3dv1t object in memory. """
+
+    def __init__(self):
+
+        self.obj_id: str = ''
+        self.pt_data: bytes = b''
+        self.ct_segments: list[CTSegment] = []
 
 # ------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------------------
@@ -23,20 +41,20 @@ class CTSegment:
     def __init__(self, idx: int = 0, ct_chunk: bytes = b""):
 
         # 'i' key in meta_dict - offset of this segment in the original file
-        self.idx = idx
+        self.idx: int = idx
 
         # 'o' key in meta_dict - the object id of the parent vault object this segment belongs to.
-        self.parent_obj_id = ''  
+        self.parent_obj_id: str = ''
 
         # krypt mode for this segment, e.g. 'km_1' or 'km_2' - one of these strings will be present in the meta_dict
-        self.km = None
+        self.km: RVKryptMode | None = None
 
         # 'km_1' or 'km_2' ... key in meta_dict - value for this key will be this dict. which contains data specific to this km
-        self.km_data = {}
+        self.km_data: dict = {}
 
         # not in meta_dict, ct_chunk is the frame payload.
-        self.ct_chunk = ct_chunk  # ciphertext segment
-    
+        self.ct_chunk: bytes = ct_chunk  # ciphertext segment
+
     def __str__(self):
         " str rep for debugging purposes. "
 
@@ -47,12 +65,17 @@ class CTSegment:
                f"km_data={self.km_data}, \n" \
                f"ct_chunk='{self.ct_chunk.hex()[:5]}...'\n"
 
+
 # ------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------------------
 class RVKryptMode(Enum):
+    """ Enum for the different encryption modes used in the vault. """
+
+    # PT = "km_0"  # plaintext, no encryption
     CHACHA20_POLY1305 = "km_1"
     FERNET = "km_2"
     # TODO: look into github.com/tink-crypto/tink-py
+
 
 # ------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------------------
@@ -72,6 +95,6 @@ class VaultKeys:
         " str rep for debugging purposes. "
 
         return f"VaultKeys: \n" \
-               f"osfp_key='{self.osfp_key.hex()[:5]}...', \n" \
-               f"sgk_chacha20_poly1305='{self.sgk_chacha20_poly1305.hex()[:5]}...', \n" \
-               f"sgk_fernet='{self.sgk_fernet.hex()[:5]}...'\n"
+               f"osfp_key='{self.osfp_key.hex()[:4]}...', \n" \
+               f"sgk_chacha20_poly1305='{self.sgk_chacha20_poly1305.hex()[:4]}...', \n" \
+               f"sgk_fernet='{self.sgk_fernet.hex()[:4]}...'\n"
